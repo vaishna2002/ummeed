3c370a725a35ba5ebeac616fbae25e563c3f0f23
Bhavesh Anandpara <bhaveshanandpara12@gmail.com>
2023-10-19 13:39:47 +0530

    Router Created

---
+++ client/src/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+
+export default function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+
+          {/* Student Dashboard */}
+
+          <Route path="/" element={<Home />} />
+          <Route path="/connect" element={<Connect />} />
+          <Route path="/task" element={<Task />} />
+          <Route path="/logout" element={<Logout />} />
+
+          {/* College Dashboard */}
+
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/connect" element={<Analytics />} />
+          <Route path="/logout" element={<Logout />} />
+
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
